8698998aab63f9fefd4a3f50649c48ade7836d69
MuhammedMusharaf007 <mohammedmusharafnilambur@gmail.com>
2022-05-25 21:20:20 +0530

    Added documantation and final readme file

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 HW Saver LLP
 Machine Learning internship
+----------------------------------
+1. The file skills_copy.py is the python file with the ML development and result prediction algorithm.
+
+2. Libraries used : Pandas, csv, Sci-Kit Learn
+
+3. The file 'Example_Technical_skills' was used as a reference to create a training sample. The copy of the file was created to which more soft skills, technical skills and random phrases were added to create a training dataset of 3000+ data samples.
+
+4. The file was imported into the program using Pandas Library.
+
+5. After selecting the column required to assess the data, a pipeline was created consisting of CountVectorizer to vectorize the words and RandomForestClassifier to create text classification model. 
+
+6. The Dataset was passed into it and the ML model was created which has 85 (+-2)% Accuracy was created.
+
+7. The Prediction file was imported along with the creation of the files to write down the classified Technical skills and Non technical skills.
+
+8. The prediction file was iterated over through the prediction of the pipeline and the ones identified as technical skills were written to the Technical-skills file and the non technical ones into the Soft-skills file.
+
+----------------------------------------
+Created by @MuhammedMusharaf007
--- skills_copy.py
+++ skills_copy.py
@@ -1,42 +1,70 @@
+# Importing the necessary Libraries
+
+# Pandas to read CSV files
 import pandas as pd
+
+# CSV to write csv files
 from csv import writer
+
+# Sci-kit learn to use CountVectorizer and Random Forest Classifier in the Pipeline we create
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline, make_pipeline
 
-
+# Importing and reading the Dataset file
 d = pd.read_csv(r'/home/vostroraf/Documents/HW_Saver_internship/Files/Example_Technical_Skills (copy).csv')
 print(len(d.query('value == 1')))
-# d_shuf = d.sample(frac=1)
 d_shuf = d
+
+# Taking the column which has the important data required
 d_content = d_shuf['Technology Skills']
 d_label = d_shuf['value']
 print(d_label)
+
+# Creating the Vectorizer instance
 vectorizer = CountVectorizer()
 
+# Creating the pipeline instance
 pipeline = Pipeline([
     ('bag-of-words', CountVectorizer()),
     ('random forest', RandomForestClassifier()),
     ])
-make_pipeline(CountVectorizer(), RandomForestClassifier(n_estimators=1000))
+
+# Creating the pipeline with countVectorizer and RandomForestClassifier
+make_pipeline(CountVectorizer(), RandomForestClassifier(n_estimators=1000)) # n_estimators can be adjusted on a trial and error method
 pipeline.fit(d_content, d_label)
+
+# Checking whether the model is trained by an obvious scorecheck (will return 100 percent accuracy since we used all training data to creare ML model)
 print(pipeline.score(d_content[3000:], d_label[3000:]))
 
+# Importing the testing Dataset to split into technical and soft skills.
 f1 = pd.read_csv(r'/home/vostroraf/Documents/HW_Saver_internship/Files/Raw_Skills_Dataset (copy).csv')
+
+# Creating files to write the data samples after recognizing whether they are Technical skills or not.
 f2 = open('/home/vostroraf/Documents/HW_Saver_internship/Technical-skills.csv', 'w')
 f3 = open('/home/vostroraf/Documents/HW_Saver_internship/Soft-skills.csv', 'w')
 
 writer2 = writer(f2)
 writer3 = writer(f3)
 
+# Iterating through the whole prediction set.
 for i in f1['RAW DATA']:
-    # print(pipeline.predict([i])[0])
+
+    # Checking whether the data is technical skills >> Adding to technical skills file.
     if pipeline.predict([i]) == 1:
         print(f"{i.split('$$')} : {pipeline.predict([i])}")
         writer2.writerow(i.split('Made by Musharaf'))
+    
+    # Adding to soft skills file if the sample is not a technical skill.
     else:
         print(f"{i.split('$$')} : {pipeline.predict([i])}")
         writer3.writerow(i.split('Made by Musharaf'))
 
+# Closing the opened files after entering.
 f2.close()
 f3.close()
+
+
+#____________________________________#
+### Created By @MuhammedMusharaf007 ##
+#------------------------------------#
